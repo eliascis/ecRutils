54b5c445a6f3697427c20b51287c410886619178
Elías Cisneros <ec@elias-cisneros.de>
2019-05-22 14:59:29 +0200

    first commit

---
+++ R/f.test.R
@@ -0,0 +1,16 @@
+#' @title Test
+#'
+#' @description Testing the world
+#'
+#' @param character
+#'
+#' @return NULL
+#'
+#' @examples f.test("blup")
+#'
+#' @export f.test
+#'
+
+f.test<-function(x){
+  print(paste("hello, ",x))
+}
