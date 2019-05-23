8d7815b52ecf68c1609da5e381632729c8992372
Elías Cisneros <ec@elias-cisneros.de>
2019-05-23 16:25:43 +0200

    foldersetup data object attached to package

---
+++ R/databasefolder.R
@@ -0,0 +1,21 @@
+#' My folders
+#'
+#' @docType data
+#'
+#' @usage data(foldersetup)
+#'
+#' @format An object of class \code{"character"}.
+#'
+#' @keywords folder on elias' os
+#'
+#' @source databasefolder <- file.path("/Users/eliascis/dropbox/kairos/research_supplements/database")
+#' gdalfolder <- ""
+#' save(databasefolder,gdalfolder, file="data/foldersetup.RData")
+#' https://kbroman.org/pkg_primer/pages/data.html
+#'
+#' @examples
+#' data(foldersetup)
+"databasefolder"
+"gdalfolder"
+
+
